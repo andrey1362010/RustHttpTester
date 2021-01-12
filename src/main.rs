c4ab36b9c401292bf285cacd03c1ff7ac378cb18
andrey1362010@yandex.ru <Ma30775136!>
2021-01-12 22:10:33 +0300

    в

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,37 @@
-extern crate hyper;
+
 use std::time::{SystemTime, UNIX_EPOCH};
-use hyper::Client;
+use std::thread;
+use std::thread::JoinHandle;
+use curl::easy::Easy;
+use curl::Error;
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let client = Client::new();
-
-    let req_count = 50000;
+fn main() {
+    let req_count = 20000;
+    const threads_count: u128 = 10;
     let start_time = time_millis();
-
-    for i in 0..req_count {
-        let uri = "http://127.0.0.1:13265/".parse()?;
-        let resp = client.get(uri).await?;
-        //println!("Response: {}", resp.status());
+    let mut join_array: Vec<JoinHandle<()>> = vec![];
+    for t in 0..threads_count {
+        let mut handle =
+            thread::spawn(move || {
+                let mut handle = Easy::new();
+                for i in 0..req_count {
+                    handle.url("http://127.0.0.1:13265/").unwrap();
+                    match handle.perform(){
+                        Ok(_) => {}
+                        Err(err) => {print!("{}", err)}
+                    }
+                    //let resp = reqwest::blocking::get("http://127.0.0.1:13265/").unwrap();
+                }
+                println!("Thread done:{}", t);
+            });
+        join_array.push(handle);
+    }
+    for handle in join_array {
+        handle.join().unwrap();
     }
     let end_time = time_millis();
-    println!("Total speed req/sec:{}", (1000 * req_count / (end_time - start_time)));
-    Ok(())
+    println!("Total speed req/sec:{}", threads_count * (1000 * req_count / (end_time - start_time)));
+    return ();
 }
 
 fn time_millis() -> u128 {
